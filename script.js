355b5a879c91dad545fc539448aa0ce07b775c5e
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-13 14:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+
+
+var table=document.getElementById("myTable");
 function createTable() {
-    //Write your code here
-  
+	let row=prompt();
+	let col=prompt();
+	for (let i = 0; i < row; i++) {
+		var rows = document.createElement("tr");
+		for (let j = 0; j < col; j++) {
+		var cols=document.createElement("td");
+			cols.innerHTML = `Row-${i} Column-${j}`;
+           rows.appendChild(cols);
+		}
+		table.appendChild(rows);
+	}
+		
+	
+	
 }
